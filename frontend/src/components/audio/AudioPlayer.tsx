56191af777394312fbf05152c41ba414da88754f
andrewwillette <willette.andrew1846@gmail.com>
2021-09-16 22:07:20 -0500

    master: adding text-decoration : none to soundcloud api widget

--- frontend/src/components/audio/AudioPlayer.tsx
+++ frontend/src/components/audio/AudioPlayer.tsx
@@ -12,7 +12,8 @@ export class AudioPlayer extends Component<any, any> {
                         soundcloud: {
                             // should work according to docs https://github.com/CookPete/react-player but it's borked. I shove the data in url as query param lmao ??
                             options : {
-                                show_user: false
+                                show_user: false,
+                                text-decoration: none
                             }
                         }
                     }}
