90564a88f2624951b9baab0d5014b2621814fdd2
andrewwillette <willette.andrew1846@gmail.com>
2021-10-11 21:39:29 -0500

    master: updating resume

--- frontend/src/components/resume/ResumePage.tsx
+++ frontend/src/components/resume/ResumePage.tsx
@@ -5,88 +5,42 @@ export class ResumePage extends Component<any, any> {
     render() {
         return (
             <div id="resume-page">
-
-                <h3 className="resume-header">
-                    Employment
-                </h3>
-                <ul id="resume-employment">
-                    <li>
-                        Cerner 2017 - 2021
-                        <ul>
-                            <li>
-                                Software Engineer II - Cloud Identity and Access Management
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        GOM Software International 2018 - 2020
-                        <ul>
-                            <li>
-                                Software Engineer - Agricultural Feeding
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        Fiddling Technologies 2017 - 2021
-                        <ul>
-                            <li>
-                                President
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
                 <h3 className="resume-header">
                     Software Development Experience
                 </h3>
                 <ul>
                     <li>
-                        Java
+                        Cerner - Software Engineer II - Cloud Identity and Access Management - 2017 - 2021
                         <ul>
                             <li>
-                                Added support in Cerner's User Roster backend and frontend services for inactive users, rather than deleting resources.
+                                Added support in enterprise User Roster for inactive users, allowing for persistence of old account resources rather than deleting them.
                             </li>
                             <li>
-                                Added support in Cerner's Authorization Server for JSON Web Key Set authentication. Includes the work to allow registering JSON Web Key Sets to pair with the signed JWT's presented to the Authorization Server.
+                                Added enterprise support for JSON Web Token(JWT) authentication. Configured application-registered accounts to allow registration of JSON Web Key Sets and Authorization Server validation of associated signed JWTs.
                             </li>
                             <li>
-                                Updated Cerner's System Accounts application to allow for lower-privileged third-party System Accounts automatic registration. Part of 21st Century Cures Act for allowing greater interoperability of Cerner's APIs.
+                                Updated enterprise System Accounts solution to allow for automatic registration of lower-privileged third-party accounts. Part of compliance with 21st Century Cures Act regulations for allowing greater interoperability of EHR APIs.
                             </li>
                             <li>
-                                Implemented New Relic and Google Analytics Metrics client library in many different Tomcat-hosted cloud solutions. Both Counters and Gauges configured for UI interaction and website performance.
+                                Implemented New Relic and Google Analytics client libraries in many different Tomcat-hosted cloud solutions. Both Counters and Gauges configured for UI interaction and website performance.
                             </li>
-                            <li>
-                                Primary Java technologies include Maven, Spring, JAXRS, HK2, Hibernate, Tomcat, OracleDB, JSP, JUnit, and Spock.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        Javascript / Typescript
-                        <ul>
                             <li>
                                 Built React frontends from wireframes for sites supporting identity-access services, such as logging into identity providers and managing user sessions.
                             </li>
                             <li>
-                                Wrote javascript consuming Cloud IAM Graph-QL endpoints for mapping clients to their identity-providers.
+                                Wrote javascript client code for consuming Cloud IAM Graph-QL endpoints which facilitated mapping clients to their identity-providers.
                             </li>
                             <li>
-                                Implemented npm Typescript client library for third-party apps to authenticate with Cerner Single-Sign-On sessions.
+                                Implemented npm Typescript client library used by internal and third-party apps for managing authentication with Cerner Single-Sign-On solution.
                             </li>
                             <li>
-                                Extensive css experience styling login pages, user tables, and account customization UIs.
+                                Extensive CSS experience styling login pages, user tables, and account customization UIs.
                             </li>
                             <li>
-                                Extensive testing experience in the npm environment (mocha, jest, chai, jasmine, enzyme).
+                                Managed linux server deployments with Chef Infrastructure Management. Good understanding of centralized Chef server and per-service cookbook/role configurations.
                             </li>
                             <li>
-                                Personal website built on Typescript-based React front end.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        AWS
-                        <ul>
-                            <li>
-                                Experience writing and managing Terraform scripts for EC2 instances, Elastic Load Balancers, Target Groups, and Security Groups in a multi-cloud-region deployment pattern.
+                                Wrote and managed Terraform scripts for EC2 instances, Elastic Load Balancers, Target Groups, and Security Groups in a multi-cloud-region deployment pattern.
                             </li>
                             <li>
                                 Experience writing and managing Packer configurations for per-service AMIs.
@@ -95,16 +49,19 @@ export class ResumePage extends Component<any, any> {
                                 Integrated AWS environment with Jenkins master / slaves deployments. Using native jenkins groovy API's and AMI's managed via Packer, all jenkins builds operated in AWS infrastructure.
                             </li>
                             <li>
-                                Allowed passivity in cloud migrations via customized Elastic Load Balancers rules to route legacy traffic to newly supported resource locations.
+                                Extensive testing experience in the npm environment (mocha, jest, chai, jasmine, enzyme) and java environment (failsafe and surefire).
                             </li>
                             <li>
-                                Strong familiarity with AWS billing practices / patterns.
+                                Primary Java technologies used include Maven, Spring, JAXRS, HK2, Hibernate, Tomcat, OracleDB, JSP, JUnit, and Spock.
                             </li>
                         </ul>
                     </li>
                     <li>
-                        C#
+                        GOM Software International - Software Engineer - Agricultural Feeding, 2018 - 2020
                         <ul>
+                            <li>
+                                Full stack .NET Core 2.0 development using razor frontends.
+                            </li>
                             <li>
                                 Implemented eight different frontend pages from wireframe specs, complete with full table functionality managed via knockout.js.
                             </li>
@@ -112,7 +69,21 @@ export class ResumePage extends Component<any, any> {
                                 Implemented Braintree online payment system collecting credit and debit cards for a scheduled payment processing plan.
                             </li>
                             <li>
-                                Managed Microsoft SQL Server schema modifications for role-based authentication.
+                                Extended Microsoft SQL Server schema for managing livestock feeding data.
+                            </li>
+                            <li>
+                                Implemented role-based authentication.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Fiddling Technologies - President, 2017 - 2021
+                        <ul>
+                            <li>
+                                Personal website built on Golang rest API server and Typescript React Single-Page-Application front-end.
+                            </li>
+                            <li>
+                                Managed amateur violin performances around Kansas City, primarily coffee shops and busking.
                             </li>
                         </ul>
                     </li>
@@ -120,30 +91,23 @@ export class ResumePage extends Component<any, any> {
                         Miscellaneous
                         <ul>
                             <li>
-                                Linux (Rhel, Ubuntu)
+                                Editor workflows: Vim, IntelliJ Idea.
                             </li>
                             <li>
-                                Bash, Golang, Python
+                                Strong unix knowledge with Linux (Ubuntu, RHEL) and MacOS.
                             </li>
                             <li>
-                                IntelliJ Idea, Visual Studio, Vim
+                                Additional development experience with Bash, Golang, Python, Ruby, and C++.
                             </li>
                             <li>
-                                Docker
+                                Moderate containerization experience with Docker.
                             </li>
                             <li>
-                                New Relic
-                            </li>
-                            <li>
-                                GraphQL
-                            </li>
-                            <li>
-                                Chef CI/CD Infrastructure
+                                Strong knowledge configuring applications with New Relic and Splunk for metrics and troubleshooting.
                             </li>
                         </ul>
                     </li>
                 </ul>
-
                 <h3 className="resume-header">
                     Education: University of Iowa, 2018
                 </h3>
