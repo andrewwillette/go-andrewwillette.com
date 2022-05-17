fa6d94dd49c491657ee896bde4375a1b7a87ea61
andrewwillette <willette.andrew1846@gmail.com>
2022-05-17 17:30:46 -0500

    master: useless comment

--- scripts/initAdmin.sh
+++ scripts/initAdmin.sh
@@ -2,6 +2,7 @@
 read -p "username: " username
 read -p "password: " password
 
+# if running in docker deployment
 if [[ -d /goApp/db ]]
 then
 	sqlite3 /goApp/db/sqlite-database.db "INSERT INTO userCredentials(username, password) VALUES('$username', '$password');"
